244acbb27ff2a9928b6f744de12b9191ac5347f4
Vira <vira.romanko99@gmail.com>
2020-02-26 23:51:16 -0500

    vue components

---
+++ js/components/HomeComponent.js
@@ -0,0 +1,194 @@
+export default {
+    template: `
+    <div>
+    <div class="jumbotron">
+    <div class="hero  align-items-center row flex-wrap">
+
+      <h1 class="display-4 col-md title"><span>DO NOT BE </span> <span class="darker">AFRAID </span><span>TO
+          KNOW</span></h1>
+      <div class="characters col-md-7">
+        <div class="d-flex crh-box row justify-content-center">
+          <!-- <object width="100%" data="images/hero_1.svg" type="image/svg+xml" class="img-fluid row "></object> -->
+          <object width="100%" data="images/hero_2.svg" type="image/svg+xml" class="img-fluid row"></object>
+          <object width="100%" data="images/hero_3.svg" type="image/svg+xml" class="img-fluid row"></object>
+          <object width="100%" data="images/hero_4.svg" type="image/svg+xml" class="img-fluid row"></object>
+          <object width="100%" data="images/heor_5.svg" type="image/svg+xml" class="img-fluid row"></object>
+
+
+        </div>
+      </div>
+
+
+
+      <div class="d-flex col-0 social social-animation mt-md-0">
+        <object data="images/youtube.svg" type="image/svg+xml" class="img-fluid row "></object>
+        <object data="images/twitter.svg" type="image/svg+xml" class="img-fluid row "></object>
+        <object data="images/snapchat.svg" type="image/svg+xml" class="img-fluid row "></object>
+        <object data="images/instagram.svg" type="image/svg+xml" class="img-fluid row "></object>
+        <object data="images/facebook.svg" type="image/svg+xml" class="img-fluid row "></object>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="numbers">
+      <h2 class=" display-4 main-title text-center mt-5">HIV NUMBERS</h2>
+      
+
+    
+      <div class="d-flex red-box-parent row justify-content-center flex-wrap mt-4">
+        <div data-aos="zoom-in-up" data-aos-duration="2000" data-aos-easing="ease-in-out"  class="red-bg col-lg-4">
+          <img class="img-fluid" src="images/red_bg.svg" alt="red bg">
+          <h4 class="header-numbers">24 500 000</h4>
+          <p>people were accessing
+            antiretroviral therapy</p>
+        </div>
+
+        <div data-aos="zoom-in-up" data-aos-duration="2000" data-aos-easing="ease-in-out" data-aos-delay="50" class="red-bg col-lg-4">
+          <img class="img-fluid" src="images/red_bg_2.svg" alt="red bg 2">
+          <h4 class="header-numbers">37 900 000</h4>
+          <p>people globally were
+            living with HIV
+          </p>
+        </div>
+
+        <div data-aos="zoom-in-up" data-aos-duration="2000" data-aos-easing="ease-in-out" data-aos-delay="100" class="red-bg col-lg-4 ">
+          <img class="img-fluid" src="images/red_bg_3.svg" alt="red bg 3">
+          <h4 class="header-numbers">17 000 000</h4>
+          <p>people became newly
+            Infected with HIV</p>
+        </div>
+
+      </div>
+  
+
+
+
+    </div>
+
+    <div class="know">
+      <h2 class=" display-4 main-title text-center mt-5">DO YOU KNOW...</h2>
+      <div class="black-box" data-aos="fade-right"  data-aos-duration="2000">
+        <img src="images/black_box.jpg" alt="black-box"/>
+        <h4 class="float-right">... what HIV/AIDS is?</h4>
+        <p class="float-right"> HIV stands for Human Immunodeficiency Virus. It is a virus that attacks the immune
+          system. It attacks and destroys white blood cells and makes copies of itself in these cells, which are our
+          body’s natural defence against illness. </p>
+          <p class="more-info float-right mr-4">
+            
+            <a href="#" class="btn btn-red btn-animation-1">More info</a></p>
+
+          <object data="images/yellow_character.svg" type="image/svg+xml" class="img-fluid row "></object>
+
+      </div>
+      <div class="black-box left-box" data-aos="fade-left"  data-aos-duration="2000">
+        <img src="images/black_box_left.jpg" alt="black-box"/>
+        <h4>...how people are living with HIV?</h4>
+        <p> Currently, over 30% of all new HIV infections globally are estimated to occur among youth ages 15 to 25
+          years. Also, increasingly, children infected at birth grow into adolescents who have to deal with their HIV
+          positive status. Combining the two, there is 5 million youth living with HIV.
+        </p>
+        <p class="more-info float-right mr-4">
+            
+          <a href="#" class="btn btn-red btn-animation-1">More info</a></p>
+
+        <object  data="images/purple_character.svg" type="image/svg+xml" class="img-fluid d-flex mr-auto"></object>
+
+      </div>
+       <div class="black-box" data-aos="fade-right"  data-aos-duration="2000">
+         <img src="images/black_box.jpg" alt="black-box"/>
+
+        <h4 class="float-right">... what HIV/AIDS is?</h4>
+        <p class="float-right"> HIV stands for Human Immunodeficiency Virus. It is a virus that attacks the immune
+          system. It attacks and destroys white blood cells and makes copies of itself in these cells, which are our
+          body’s natural defence against illness. </p>
+          <p class="more-info float-right mr-4">
+            
+            <a href="#" class="btn btn-red btn-animation-1">More info</a></p>
+
+          <object data="images/red_character.svg" type="image/svg+xml" class="img-fluid row "></object>
+
+      </div>
+
+    </div>
+
+   
+    <div class="protect">
+
+      <div class="d-flex row box-blue-bg justify-content-center mt-4">
+        <img data-aos="fade-up" data-aos-duration="3000" class="img-fluid" src="images/blue_box.jpg" alt="box svg">
+        
+        
+        <h2 class=" display-4 main-title white-t text-center mt-5">PROTECT YOURSELF</h2>
+
+        <p>Protecting yourself from HIV begins with understanding how the virus is spread.
+          The virus can be passed in only certain ways: During sex with a person infected with HIV;
+          By sharing a contaminated needle, such as through illicit drug use …</p>
+          <p class="row justify-content-center link-protect">
+            
+              <a href="#" class="btn btn-white btn-animation-1">Safe sex</a>
+         
+            
+              <a class="btn btn-white btn-animation-1" href="#">Safe drug use</a>
+           
+            
+          </p>
+         
+
+          <object data-aos="fade-up" data-aos-duration="2000"data="images/blue-char.svg" type="image/svg+xml" class="img-fluid row "></object>
+
+
+
+      </div>
+    </div>
+   
+    <div class="test">
+      <h2 class=" display-4 main-title text-center  mb-0">UP TO TEST?</h2>
+      <div data-aos="zoom-in-left" data-aos-duration="2000"  class="d-flex   justify-content-center mt-4">
+        <div class=" box-bg">
+          <img class="img-fluid" src="images/box.svg" alt="box svg">
+          <h4>What is HIV test?
+          </h4>
+          <p>The HIV test is a blood test. There are two ways this test can be done.
+            Rapid tests, also known as “point-of-care” tests, offer results within minutes after blood is taken,
+            usually from a fingerprick.
+            Standard tests usually involve blood being taken from a vein in the arm. The blood is then sent to a lab
+            for testing. The results take two weeks… </p>
+            <p class="link-test">
+              <a href="#" class="btn btn-red btn-animation-1">More info</a>
+            </p>
+          
+        </div>
+      </div>
+    </div>
+   
+    <div class="news">
+      <h2 class=" display-4 main-title text-center mt-5">LATEST NEWS</h2>
+      <div class="d-flex flex-wrap justify-content-center m-4">
+        <div class="text-center p-3">
+          <img class="img-fluid news-img" src="images/news.svg" alt="news icon" />
+          <h4 class="subtitle text-center">NEW RESEARCH ABOUT HIV</h4>
+          <p>JANUARY 30, 2020</p>
+        </div>
+        <div class="text-center p-3">
+          <img class="img-fluid news-img" src="images/news_1.svg" alt="news icon" />
+          <h4 class="subtitle">DOCTORS THAT SAVE LIVES</h4>
+          <p>JANUARY 30, 2020</p>
+        </div>
+        <div class="text-center p-3">
+          <img class="img-fluid news-img" src="images/news_3.svg" alt="news icon" />
+          <h4 class="subtitle">FDA-Approved HIV Medicines</h4>
+          <p>JANUARY 30, 2020</p>
+        </div>
+      </div>
+
+    </div>
+   
+
+  </div>
+ 
+</div>
+</div>
+
+    `
+}
